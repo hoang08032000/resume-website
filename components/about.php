7d96c093cf4945de16be3092702d1853f61172ac
hoangtoviet <hoangtv@dyno.vn>
2022-06-19 07:52:33 +0700

    chore: sql

--- components/about.php
+++ components/about.php
@@ -39,7 +39,7 @@ if (mysqli_num_rows($user) > 0 && mysqli_num_rows($edu) > 0) {
 
 <section class="home bd-grid" id="home">
     <div class="home__data">
-        <h2 class="home__title">Xin chào,<br>Mình là <span class="home__title-color"><?php echo $lastname ?></span><br> Backend Developer </h2>
+        <h2 class="home__title">Xin chào,<br>Mình là <span class="home__title-color"><?php echo $lastname ?></span><br> Web Developer </h2>
 
         <a href="index.php?page=send-mail&id=<?php echo $user_id ?>" class="button">Mail me</a>
     </div>
